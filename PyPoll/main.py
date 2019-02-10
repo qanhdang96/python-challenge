6b7d878958d3a73c238da308baab27e870830c04
Jenny Dang <qanhdang96@hotmail.com>
2019-02-09 20:21:41 -0600

    final

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,12 +1,12 @@
 import os 
 import csv 
 
-#Path to collect data from the Resources folder 
+#Path to collect data from the folder 
 csvpath = os.path.join('election_data.csv')
 
 #Define the variables 
 total = 0
-candidate = []
+candidate_list = []
 khan_votes = 0 
 correy_votes = 0 
 li_votes = 0 
@@ -21,11 +21,14 @@ with open(csvpath, newline='') as electiondata:
     
     # Loop through the data 
     for row in csvreader:
+
         #Count the total votes  
         total += 1
-        #Append the candidate to their list 
-        candidate.append(row[2])
+
+        #Append the candidate to their list, we will need this list later to match the election winner
+        candidate_list.append(row[2])
         
+        # Use Conditionals to find percentage and total votes of each candidate 
         if ("Khan" == row[2]):
             khan_votes += 1
             khan_percent = (khan_votes/total)*100
@@ -38,12 +41,14 @@ with open(csvpath, newline='') as electiondata:
         elif ("O'Tooley" == row[2]): 
             tooley_votes += 1
             tooley_percent = (tooley_votes/total)*100
-            
+
+# Create a list to store all of the candidates' total votes             
 candidate_votes = [khan_votes, correy_votes, li_votes, tooley_votes]
 
+# Using max function, we can find the greatest number of votes within candidate votes list 
 winner = max(candidate_votes)
 
-# Print out stats for budget data 
+# Print out stats for Election data
 print("Election Results")
 print("----------------------------")
 print(f"Total Votes: {total}")
@@ -53,5 +58,19 @@ print(f"Correy: {correy_percent:.3f}% ({correy_votes})")
 print(f"Li: {li_percent:.3f}% ({li_votes})")
 print(f"O'Tooley: {tooley_percent:.3f}% ({tooley_votes})")
 print("----------------------------")
-print(f"Winner: {candidate[winner]}")
+print(f"Winner: {candidate_list[winner]}")
 print("----------------------------")
+
+#Export a text file with the results 
+file = open('Election_Results.txt','w')
+file.write("Election Results" + "\n")
+file.write("----------------------------" + "\n")
+file.write(f"Total Votes: {total}"+ "\n")
+file.write("----------------------------" + "\n")
+file.write(f"Khan: {khan_percent:.3f}% ({khan_votes})" + "\n")
+file.write(f"Correy: {correy_percent:.3f}% ({correy_votes})" + "\n")
+file.write(f"Li: {li_percent:.3f}% ({li_votes})" + "\n")
+file.write(f"O'Tooley: {tooley_percent:.3f}% ({tooley_votes})" + "\n")
+file.write("----------------------------" + "\n")
+file.write(f"Winner: {candidate_list[winner]}" + "\n")
+file.write("----------------------------" + "\n")
